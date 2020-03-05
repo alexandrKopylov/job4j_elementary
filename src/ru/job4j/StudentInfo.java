3023055dcf162774d4d6a9420781cedc588842c1
alexx33915 <sash-2007@ya.ru>
2020-03-05 14:35:18 +0500

    3.3. Разбор первой программы.[#246460]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Александр Копылов");
+        System.out.println("08.11.1980");
+    }
+}
