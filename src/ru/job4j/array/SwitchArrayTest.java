344567f3ca8f720fc28b8cac0646659fff517782
alexx33915 <sash-2007@ya.ru>
2020-03-23 20:18:38 +0500

    6.2.2. Переставить элементы массива[#246434]

--- src/ru/job4j/array/SwitchArrayTest.java
+++ src/ru/job4j/array/SwitchArrayTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class SwitchArrayTest {
+
     @Test
     public void whenSwap0to3() {
         int[] input = {1, 2, 3, 4};
