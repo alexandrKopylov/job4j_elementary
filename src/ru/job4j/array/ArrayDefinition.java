f6649adbb41c1bbc6f7b6d45f0a4f741511bf5c2
alexx33915 <sash-2007@ya.ru>
2020-03-23 12:40:12 +0500

    6.0.1. Объявление массива.[#246437]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -2,8 +2,9 @@ package ru.job4j.array;
 
 public class ArrayDefinition {
 
-
-    Short[] ages = new Short[10];
-    String[] surnames = new String[100500];
-    Float[] prices = new Float[40];
+    public static void main(String[] args) {
+        Short[] ages = new Short[10];
+        String[] surnames = new String[100500];
+        Float[] prices = new Float[40];
+    }
 }
