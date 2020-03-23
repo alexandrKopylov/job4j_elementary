24b95db603c52ed7270082d5d492f9834eed92c7
alexx33915 <sash-2007@ya.ru>
2020-03-23 10:24:14 +0500

    package-info

---
+++ src/ru/job4j/calculator/package-info.java
@@ -0,0 +1,7 @@
+        /**
+        * Package for calculate task.
+        * @author Alexandr Kopylov
+        * @version 0.1
+        * @since 0.1
+        */
+        package ru.job4j.calculator;
