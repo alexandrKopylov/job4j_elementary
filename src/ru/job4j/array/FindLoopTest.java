161a0d7fe5bac6a123739d0ba47ffc103896cf87
alexx33915 <sash-2007@ya.ru>
2020-03-24 17:33:50 +0500

    6.5.3. Сортировка выборкой.[#246423]

--- src/ru/job4j/array/FindLoopTest.java
+++ src/ru/job4j/array/FindLoopTest.java
@@ -41,5 +41,27 @@ public class FindLoopTest {
         int result = FindLoop.indexOf(input, value, start, finish);
         int expect = 3;
         assertThat(result, is(expect));
+
+    }
+
+    @Test
+    public void whenFind55() {
+        int[] input = new int[] {1, 2, 3, 4, 5};
+        int value = 5;
+        int start = 5;
+        int finish = 5;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = 5;
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void whenFind545() {
+        int[] input = new int[] {43, 113, 77};
+        int value = 77;
+        int start = 1;
+        int finish = 2;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = 2;
+        assertThat(result, is(expect));
     }
 }
