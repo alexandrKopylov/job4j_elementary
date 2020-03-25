6b6a24f6a4782f43b49bcff894f32d953449de94
alexx33915 <sash-2007@ya.ru>
2020-03-25 15:26:45 +0500

    turn

--- src/ru/job4j/array/Turn.java
+++ src/ru/job4j/array/Turn.java
@@ -1,16 +1,16 @@
 package ru.job4j.array;
 
 public class Turn {
+
     public static int[] back(int[] array) {
-        if(array.length==1) return array;
 
-        int polDlinni = (int)array.length/2;
+        int polDlinni = (int) array.length / 2;
 
-            for ( int i=0;i<polDlinni;i++){
-            int n= array[i];
-            array[i]=array[array.length-i-1];
-            array[array.length-i-1] = n;
-            }
+        for (int i = 0; i < polDlinni; i++) {
+            int n = array[i];
+            array[i] = array[array.length - i - 1];
+            array[array.length - i - 1] = n;
+        }
 
         return array;
     }
