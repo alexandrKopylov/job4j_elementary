ef940a21ea22be670b67249d66a51da6e3e4ce70
alexx33915 <sash-2007@ya.ru>
2020-04-19 17:47:03 +0500

    1.4. Вызов метод с возвращаемым типом.[#246589]

--- src/ru/job4j/oop/Cat.java
+++ src/ru/job4j/oop/Cat.java
@@ -1,7 +1,32 @@
 package ru.job4j.oop;
 
 public class Cat {
+
+    private String food;
+    private String name;
+
+    void giveNick(String nick){
+        this.name = nick;
+    }
+
+    public void show() {
+        System.out.println("Котик по кличке " + this.name +" ест "+  this.food);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
     public static void main(String[] args) {
-        Cat peppy = new Cat();
+
+        Cat gav = new Cat();
+        gav.giveNick("Гав");
+        gav.eat("kotleta");
+        gav.show();
+
+        Cat black = new Cat();
+        black.giveNick("Черныш");
+        black.eat("fish");
+        black.show();
     }
 }
