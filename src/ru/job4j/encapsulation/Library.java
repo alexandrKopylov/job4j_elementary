de07de92e14050c35763270474f7a67e1cb34841
alexx33915 <sash-2007@ya.ru>
2020-04-28 15:14:57 +0500

    3. Массивы и модели.[#246603]

---
+++ src/ru/job4j/encapsulation/Library.java
@@ -0,0 +1,32 @@
+package ru.job4j.encapsulation;
+
+public class Library {
+    public static void main(String[] args) {
+        Book[] books = new Book[4];
+        books[0] = new Book("Bukvar", 10);
+        books[1] = new Book("Azbuka", 15);
+        books[2] = new Book("Matematika", 55);
+        books[3] = new Book("Clean code", 100);
+
+        for(int i=0;i<books.length;i++) {
+            System.out.println(books[i].getName() + " " + books[i].getCount());
+        }
+        System.out.println();
+
+        Book tmp = books[0];
+        books[0] = books[3];
+        books[3] = tmp;
+
+        for (int i = 0; i < books.length; i++) {
+            System.out.println(books[i].getName() + " " + books[i].getCount());
+        }
+        System.out.println();
+
+        for (int i = 0; i < books.length; i++) {
+
+             if(books[i].getName()=="Clean code"){
+              System.out.println(books[i].getName() + " " + books[i].getCount());
+           }
+        }
+    }
+}
