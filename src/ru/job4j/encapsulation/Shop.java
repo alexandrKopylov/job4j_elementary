e8925ed085567455640a1e6ddf1fab13a5dbdc89
alexx33915 <sash-2007@ya.ru>
2020-05-02 17:21:50 +0500

    3. Удаление моделей из массива.[#246605]

---
+++ src/ru/job4j/encapsulation/Shop.java
@@ -0,0 +1,46 @@
+package ru.job4j.encapsulation;
+
+public class Shop {
+
+    public static   Product[] delete( Product[] products , int index) {
+        for (int i =index; i < products.length; i++) {
+            int temp = i+1;
+           if(temp<products.length-1){
+               temp = i+1;
+           }
+           else { temp = products.length-1; }
+            products[i] = products[temp];
+        }
+        products[products.length-1] = null;
+        return products;
+
+    }
+    public  static  Product[] show( Product[] products ) {
+        System.out.println();
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+        return products;
+    }
+
+    public static void main(String[] args) {
+        Product products[] = new Product[5];
+        products[0] = new Product("Milk", 10);
+        products[1] = new Product("Bread", 4);
+        products[2] = new Product("Egg", 19);
+        products[3] = new Product("Oil", 8);
+        products[4] = new Product("Tea", 15);
+
+
+        show( products );
+        delete( products , 2);
+        show( products );
+        delete( products , 0);
+        show( products );
+    }
+}
