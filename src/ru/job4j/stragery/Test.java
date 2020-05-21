eda118710812b5437072159a9bc764735ac4ceb4
alexx33915 <sash-2007@ya.ru>
2020-05-21 18:40:57 +0500

    5.1. Тестирование. Подготовка данных.[#246553]

---
+++ src/ru/job4j/stragery/Test.java
@@ -0,0 +1,15 @@
+package ru.job4j.stragery;
+
+public class Test {
+    public static void main(String[] args) {
+        Square square = new Square();
+        //Triangle triangle = new Triangle();
+
+
+            Paint paint = new Paint();
+           // paint.draw(square);
+        paint.draw(new Triangle());
+
+
+    }
+}
